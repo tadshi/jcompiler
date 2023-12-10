2757d61b873a683c6a00fffd8477b26d03924826
tadshi <tadshi@outlook.com>
2023-12-10 23:15:36 +0800

    (feat) Add basic cli and test

--- Readme.md
+++ Readme.md
@@ -1,11 +1,14 @@
 # JCompiler
-## Author
-*yifeiHuang623* (frontend)
-*tadshi* (backend)
+## Authors
+-*yifeiHuang623* (frontend)
 
-## build
+-*tadshi* (backend)
+
+## Build
 First, get the latest build of Maven at their [website](https://maven.apache.org/download.cgi);
 
 Then you can build this project via `mvn install`.
 
 Note that as backend used sealed class and switch pattern maching, you may have to update your JDK version to 17+.
+
+And if you fully use maven, you may have to modify the 'release' tag in pom.xml or you have to install JDK 21.
--- src/main/java/com/front/App.java
+++ src/main/java/com/front/App.java
@@ -1,16 +1,32 @@
 package com.front;
 import com.front.gunit.CompUnit;
 
+import java.io.File;
 import java.io.RandomAccessFile;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
-        RandomAccessFile file = new RandomAccessFile("./src/textfile.txt", "r");
-
+        RandomAccessFile rfile;
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar jcompiler.jar <target_file>");
+            return;
+        } else if ("-v".equals(args[0]) || "--version".equals(args[0])) {
+            System.out.println("Jcompiler ver 0.2 by yifeiHuang623 and tadshi");
+            return;
+        } else if (args[0].startsWith("--test=")) {
+            String testFileName = "/test" + args[0].substring(7) + ".txt";
+            System.out.println("Testing on " + testFileName.substring(1) + "...");
+            File tempFile = File.createTempFile("jcompilertest", ".txt");
+            FileUtils.copyInputStreamToFile(App.class.getResourceAsStream(testFileName), tempFile);
+            rfile = new RandomAccessFile(tempFile, "r");
+        } else {
+            rfile = new RandomAccessFile(args[0], "r");
+        }
         // 词法分析
-        LexicalParser lexical = new LexicalParser(file);
+        LexicalParser lexical = new LexicalParser(rfile);
         Map<Integer, LexicalParser.Word> wordMap = lexical.lexicalAnalysis();
 
         GrammaticalParser grammer = new GrammaticalParser(wordMap);
